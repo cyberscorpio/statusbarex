2e1b72e82bba3a6e1ad657e251d9975546849b65
doudehou <doudehou@850d1033-b77e-6cc6-6385-16b8505373e6>
2011-06-01 16:34:12 +0000

    开始用 js-ctypes 改写 XPCOM 组件，binary 的实在是跟不上 firefox 你大爷现在升级的节奏了啊。。。

--- trunk/xpi/xpi.bat
+++ trunk/xpi/xpi.bat
@@ -1,26 +1,45 @@
 @echo off
-if not exist ..\native\win32\Release\statusbarex.dll goto dllmissing
-if not exist ..\native\win32\IStatusbarExCore.xpt goto xptmissing
-@copy ..\native\win32\Release\statusbarex.dll components
-@copy ..\native\win32\IStatusbarExCore.xpt components
 @set path="C:\Program Files\7-Zip";%path%
+REM create tags first
+ctags -R *
+REM create the xpt then
+call build-idl.bat
 if exist statusbarex.xpi goto deletexpi
 
 : createxpi
-call 7z.exe a statusbarex.xpi * -r -x!xpi.bat -x!*.tmp -x!*.swp -x!*.svn -x!*.zip
+call 7z.exe a statusbarex.xpi * -r -x!*.bat -x!*.tmp -x!*.swp -x!*.svn -x!*.zip -x!*.git -x!*.php -x!tags
 @goto end
 
 :deletexpi
 del statusbarex.xpi
 @goto createxpi
 
-:dllmissing
-echo statusbarex.dll is missing....
-@goto end
-
-:xptmissing
-echo IStatusbarExCore.xpt is missing....
-@goto end
-
 :end
 @echo on
+
+REM @echo off
+REM if not exist ..\native\win32\Release\statusbarex.dll goto dllmissing
+REM if not exist ..\native\win32\IStatusbarExCore.xpt goto xptmissing
+REM @copy ..\native\win32\Release\statusbarex.dll components
+REM @copy ..\native\win32\IStatusbarExCore.xpt components
+REM @set path="C:\Program Files\7-Zip";%path%
+REM if exist statusbarex.xpi goto deletexpi
+REM 
+REM : createxpi
+REM call 7z.exe a statusbarex.xpi * -r -x!xpi.bat -x!*.tmp -x!*.swp -x!*.svn -x!*.zip
+REM @goto end
+REM 
+REM :deletexpi
+REM del statusbarex.xpi
+REM @goto createxpi
+REM 
+REM :dllmissing
+REM echo statusbarex.dll is missing....
+REM @goto end
+REM 
+REM :xptmissing
+REM echo IStatusbarExCore.xpt is missing....
+REM @goto end
+REM 
+REM :end
+REM @echo on

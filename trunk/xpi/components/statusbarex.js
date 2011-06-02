0a202735c3c6ba791a882cd30b5e692a91565030
doudehou <doudehou@850d1033-b77e-6cc6-6385-16b8505373e6>
2011-06-02 13:50:59 +0000

    go on...

--- trunk/xpi/components/statusbarex.js
+++ trunk/xpi/components/statusbarex.js
@@ -21,94 +21,104 @@ function StatusbarExObj() {
 	this.logger = Cc['@mozilla.org/consoleservice;1'].getService(Ci.nsIConsoleService);
 	var logger = this.logger;
 
-	try {
-		// memory 
-		const MEMORYSTATUSEX = new ctypes.StructType('MEMORYSTATUSEX',
-			[
-				{dwLength: ctypes.uint32_t},
-				{dwMemoryLoad: ctypes.uint32_t},
-				{ullTotalPhys: ctypes.uint64_t},
-				{ullAvailPhys: ctypes.uint64_t},
-				{ullTotalPageFile: ctypes.uint64_t},
-				{ullAvailPageFile: ctypes.uint64_t},
-				{ullTotalVirtual: ctypes.uint64_t},
-				{ullAvailVirtual: ctypes.uint64_t},
-				{ullAvailExtendedVirtual: ctypes.uint64_t}
-			]);
-		// process memory
-		const PROCESS_MEMORY_COUNTERS = new ctypes.StructType('PROCESS_MEMORY_COUNTERS',
-			[
-				{cb: ctypes.uint32_t},
-				{PageFaultCount: ctypes.uint32_t},
-				{PeakWorkingSetSize: ctypes.uintptr_t},
-				{WorkingSetSize: ctypes.uintptr_t},
-				{QuotaPeakPagedPoolUsage: ctypes.uintptr_t},
-				{QuotaPagePoolUsage: ctypes.uintptr_t},
-				{QuotaPeakNonPagedPoolUsage: ctypes.uintptr_t},
-				{QuotaNonPagedPoolUsage: ctypes.uintptr_t},
-				{PagefileUsage: ctypes.uintptr_t},
-				{PeakPagefileUsage: ctypes.uintptr_t}
-			]);
-		// system info
-		const SYSTEM_INFO = new ctypes.StructType('SYSTEM_INFO',
-			[
-				{oemid: ctypes.uint32_t},
-				{dwPageSize: ctypes.uint32_t},
-				{lpMinApplicationAddr: ctypes.voidptr_t},
-				{lpMaxApplicationAddr: ctypes.voidptr_t},
-				{dwActiveProcessorMask: ctypes.uintptr_t},
-				{dwNumberOfProcessors: ctypes.uint32_t},
-				{dwProcessorType: ctypes.uint32_t},
-				{dwAllocationGranularity: ctypes.uint32_t},
-				{wProcessorLevel: ctypes.uint16_t},
-				{wProcessorRevision: ctypes.uint16_t}
-			]);
-		// file time
-		const FILETIME = new ctypes.StructType('FILETIME',
-			[
-				{dwLowDateTime: ctypes.uint32_t},
-				{dwHighDateTime: ctypes.uint32_t}
-			]);
+try {
+	// memory 
+	const MEMORYSTATUSEX = new ctypes.StructType('MEMORYSTATUSEX',
+		[
+			{dwLength: ctypes.uint32_t},
+			{dwMemoryLoad: ctypes.uint32_t},
+			{ullTotalPhys: ctypes.uint64_t},
+			{ullAvailPhys: ctypes.uint64_t},
+			{ullTotalPageFile: ctypes.uint64_t},
+			{ullAvailPageFile: ctypes.uint64_t},
+			{ullTotalVirtual: ctypes.uint64_t},
+			{ullAvailVirtual: ctypes.uint64_t},
+			{ullAvailExtendedVirtual: ctypes.uint64_t}
+		]);
+	// process memory
+	const PROCESS_MEMORY_COUNTERS = new ctypes.StructType('PROCESS_MEMORY_COUNTERS',
+		[
+			{cb: ctypes.uint32_t},
+			{PageFaultCount: ctypes.uint32_t},
+			{PeakWorkingSetSize: ctypes.uintptr_t},
+			{WorkingSetSize: ctypes.uintptr_t},
+			{QuotaPeakPagedPoolUsage: ctypes.uintptr_t},
+			{QuotaPagePoolUsage: ctypes.uintptr_t},
+			{QuotaPeakNonPagedPoolUsage: ctypes.uintptr_t},
+			{QuotaNonPagedPoolUsage: ctypes.uintptr_t},
+			{PagefileUsage: ctypes.uintptr_t},
+			{PeakPagefileUsage: ctypes.uintptr_t}
+		]);
+	// system info
+	const SYSTEM_INFO = new ctypes.StructType('SYSTEM_INFO',
+		[
+			{oemid: ctypes.uint32_t},
+			{dwPageSize: ctypes.uint32_t},
+			{lpMinApplicationAddr: ctypes.voidptr_t},
+			{lpMaxApplicationAddr: ctypes.voidptr_t},
+			{dwActiveProcessorMask: ctypes.uintptr_t},
+			{dwNumberOfProcessors: ctypes.uint32_t},
+			{dwProcessorType: ctypes.uint32_t},
+			{dwAllocationGranularity: ctypes.uint32_t},
+			{wProcessorLevel: ctypes.uint16_t},
+			{wProcessorRevision: ctypes.uint16_t}
+		]);
+	// file time
+	const FILETIME = new ctypes.StructType('FILETIME',
+		[
+			{dwLowDateTime: ctypes.uint32_t},
+			{dwHighDateTime: ctypes.uint32_t}
+			// {ullDateTime: ctypes.uint64_t}
+		]);
 
 
-		var kernel32 = ctypes.open('kernel32.dll');
-		var psapi = ctypes.open('psapi.dll');
+	var kernel32 = ctypes.open('kernel32.dll');
+	var psapi = ctypes.open('psapi.dll');
 
-		// GlobalMemoryStatusEx
-		var fnGlobalMemoryStatusEx = kernel32.declare('GlobalMemoryStatusEx',
-				ctypes.winapi_abi,
-				ctypes.uint32_t,
-				MEMORYSTATUSEX.ptr);
-		var msex = MEMORYSTATUSEX();
+	// GlobalMemoryStatusEx
+	var fnGlobalMemoryStatusEx = kernel32.declare('GlobalMemoryStatusEx',
+			ctypes.winapi_abi,
+			ctypes.uint32_t,
+			MEMORYSTATUSEX.ptr);
 
-		// GetCurrentProcess
-		var fnGetCurrentProcess = kernel32.declare('GetCurrentProcess',
-				ctypes.winapi_abi,
-				ctypes.voidptr_t);
-		var currentProcess = fnGetCurrentProcess();
+	// GetCurrentProcess
+	var fnGetCurrentProcess = kernel32.declare('GetCurrentProcess',
+			ctypes.winapi_abi,
+			ctypes.voidptr_t);
+	var currentProcess = fnGetCurrentProcess();
 
-		// GetSystemInfo
-		var fnGetSystemInfo = kernel32.declare('GetSystemInfo',
-				ctypes.winapi_abi,
-				ctypes.void_t,
-				SYSTEM_INFO.ptr);
-		var si = SYSTEM_INFO();
-		fnGetSystemInfo(si.address());
-		var cpuCount = si.dwNumberOfProcessors;
+	// GetProcessTimes
+	var fnGetProcessTimes = kernel32.declare('GetProcessTimes',
+			ctypes.winapi_abi,
+			ctypes.uint32_t,
+			ctypes.voidptr_t,
+			FILETIME.ptr,
+			FILETIME.ptr,
+			FILETIME.ptr,
+			FILETIME.ptr);
 
-		// GetProcessMemoryInfo
-		var fnGetProcessMemoryInfo = psapi.declare('GetProcessMemoryInfo',
-				ctypes.winapi_abi,
-				ctypes.uint32_t,
-				ctypes.voidptr_t,
-				PROCESS_MEMORY_COUNTERS.ptr,
-				ctypes.uint32_t);
-		var pmc = PROCESS_MEMORY_COUNTERS();
+	// GetSystemInfo
+	var fnGetSystemInfo = kernel32.declare('GetSystemInfo',
+			ctypes.winapi_abi,
+			ctypes.void_t,
+			SYSTEM_INFO.ptr);
+	var si = SYSTEM_INFO();
+	fnGetSystemInfo(si.address());
+	var cpuCount = si.dwNumberOfProcessors;
 
-	} catch (e) {
-		logger.logStringMessage('***********' + e + '**********');
-	}
+	// GetProcessMemoryInfo
+	var fnGetProcessMemoryInfo = psapi.declare('GetProcessMemoryInfo',
+			ctypes.winapi_abi,
+			ctypes.uint32_t,
+			ctypes.voidptr_t,
+			PROCESS_MEMORY_COUNTERS.ptr,
+			ctypes.uint32_t);
 
+
+	////////////////////////////////////////////////
+	// Memory usage 
+
+	var msex = MEMORYSTATUSEX();
 	this.GetMemoryStatus = function(total_memory, free_memory) {
 		try {
 			msex.dwLength = MEMORYSTATUSEX.size;
@@ -120,10 +130,11 @@ function StatusbarExObj() {
 		}
 	}
 
+	var pmc = PROCESS_MEMORY_COUNTERS();
 	this.GetFxMemory = function(usage, vm_size) {
 		try {
 			pmc.cb = PROCESS_MEMORY_COUNTERS.size;
-			fnGetProcessMemoryInfo(fnGetCurrentProcess(), pmc.address(), PROCESS_MEMORY_COUNTERS.size);
+			fnGetProcessMemoryInfo(currentProcess, pmc.address(), PROCESS_MEMORY_COUNTERS.size);
 			usage.value = Math.floor(pmc.WorkingSetSize / (1024 * 1024));
 			vm_size.value = Math.floor(pmc.PagefileUsage / (1024 * 1024));
 		} catch (e) {
@@ -131,13 +142,61 @@ function StatusbarExObj() {
 		}
 	}
 
+
+	////////////////////////////////////////////////
+	// CPU usage 
+
+	var ftdmy1 = FILETIME(), ftdmy2 = FILETIME(); // used by sys & fx cpu usage
+
+	var fxKt = FILETIME(), fxUt = FILETIME();
+	var fxKtNow = FILETIME(), fxUtNow = FILETIME();
+	fnGetProcessTimes(currentProcess, ftdmy1.address(), ftdmy2.address(), fxKt.address(), fxUt.address());
+	var fxTs = Date.now();
+	var fxCpuUsage = 0;
 	this.GetFxCpuUsage = function(cpu_usage) {
-		cpu_usage.value = 50;
+		try {
+			var ts = Date.now();
+			if (ts - fxTs > 500) {
+				fnGetProcessTimes(currentProcess,
+					ftdmy1.address(), ftdmy2.address(),
+					fxKtNow.address(), fxUtNow.address());
+				// logger.logStringMessage(' -----------------');
+				fxCpuUsage = getValueFromFileTime(fxKtNow) - getValueFromFileTime(fxKt) +
+					getValueFromFileTime(fxUtNow) - getValueFromFileTime(fxUt);
+				// logger.logStringMessage(' +++++++++++++++++');
+				/*
+				logger.logStringMessage('=============    ' + fxKtNow.ullDateTime + ':' + fxKt.ullDateTime + ' >>> ' + ts + ':' +  fxTs + '     ===============');
+				logger.logStringMessage('-------------    ' + fxUtNow.ullDateTime + ':' + fxUt.ullDateTime + ' >>> ' + ts + ':' +  fxTs + '     ===============');
+				*/
+				// logger.logStringMessage('-----------   ' + fxCpuUsage + '   ----------');
+				fxCpuUsage /= 100;
+				fxCpuUsage /= (ts - fxTs);
+				fxCpuUsage = Math.floor(fxCpuUsage / cpuCount);
+
+				if (fxCpuUsage > 100) {
+					fxCpuUsage = 100;
+				}
+	
+				fxTs = ts;
+				fxKt.dwLowDateTime = fxKtNow.dwLowDateTime;
+				fxKt.dwHighDateTime = fxKtNow.dwHighDateTime;
+				fxUt.dwLowDateTime = fxUtNow.dwLowDateTime;
+				fxUt.dwHighDateTime = fxUtNow.dwHighDateTime;
+			}
+	
+			cpu_usage.value = fxCpuUsage;
+		} catch (e) {
+			logger.logStringMessage('^^^^^^^^^^' + e + '^^^^^^^^^^');
+		}
 	}
 
 	this.GetSysCpuUsage = function(sys_cpu_usage) {
 		sys_cpu_usage.value = 50;
 	}
+
+
+	////////////////////////////////////////////////
+	// Ethernet
 
 	this.GetEthernetCount = function() {
 		return 1;
@@ -152,6 +211,15 @@ function StatusbarExObj() {
 		name.value = 'dummy!';
 	}
 
+} catch (e) {
+	logger.logStringMessage('***********' + e + '**********');
+}
+
+	function getValueFromFileTime(ft) {
+		var value = ctypes.UInt64.join(ft.dwHighDateTime, ft.dwLowDateTime);
+		// logger.logStringMessage('value: ' + value);
+		return value.toString() - 0;
+	}
 }
 
 StatusbarExObj.prototype = {

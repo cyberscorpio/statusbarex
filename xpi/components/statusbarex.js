edbe8de2897ef17b32efc772db29c10c7064ed77
doudehou <doudehou@850d1033-b77e-6cc6-6385-16b8505373e6>
2011-06-04 17:38:33 +0000

    使用  js-ctypes  改造貌似完毕了，呵呵。现在太晚了，明天再提交给 AMO 吧。

--- xpi/components/statusbarex.js
+++ xpi/components/statusbarex.js
@@ -68,12 +68,45 @@ try {
 		[
 			{dwLowDateTime: ctypes.uint32_t},
 			{dwHighDateTime: ctypes.uint32_t}
-			// {ullDateTime: ctypes.uint64_t}
+		]);
+	// MIB_IFROW
+	const MIB_IFROW = new ctypes.StructType('MIB_IFROW',
+		[
+			{wszName: ctypes.ArrayType(ctypes.jschar, 256)},
+			{dwIndex: ctypes.uint32_t},
+			{dwType: ctypes.uint32_t},
+			{dwMtu: ctypes.uint32_t},
+			{dwSpeed: ctypes.uint32_t},
+			{dwPhysAddrLen: ctypes.uint32_t},
+			{bPhysAddr: ctypes.ArrayType(ctypes.uint8_t, 8)},
+			{dwAdminStatus: ctypes.uint32_t},
+			{dwOperStatus: ctypes.uint32_t},
+			{dwLastChange: ctypes.uint32_t}, 
+			{dwInOctets: ctypes.uint32_t},
+			{dwInUcastPkts: ctypes.uint32_t},
+			{dwInNUcastPkts: ctypes.uint32_t},
+			{dwInDiscards: ctypes.uint32_t},
+			{dwInErrors: ctypes.uint32_t},
+			{dwInUnknownProtos: ctypes.uint32_t},
+			{dwOutOctets: ctypes.uint32_t},
+			{dwOutUcastPkts: ctypes.uint32_t},
+			{dwOutNUcastPkts: ctypes.uint32_t},
+			{dwOutDiscards: ctypes.uint32_t},
+			{dwOutErrors: ctypes.uint32_t},
+			{dwOutQLen: ctypes.uint32_t},
+			{dwDescrLen: ctypes.uint32_t},
+			{bDescr: ctypes.ArrayType(ctypes.uint8_t, 256)}
+		]);
+	const MIB_IFTABLE = new ctypes.StructType('MIB_IFTABLE',
+		[
+			{dwNumEntries: ctypes.uint32_t},
+			{table: ctypes.ArrayType(MIB_IFROW, 64)}
 		]);
 
 
 	var kernel32 = ctypes.open('kernel32.dll');
 	var psapi = ctypes.open('psapi.dll');
+	var iphlpapi = ctypes.open('iphlpapi.dll');
 
 	// GlobalMemoryStatusEx
 	var fnGlobalMemoryStatusEx = kernel32.declare('GlobalMemoryStatusEx',
@@ -97,6 +130,14 @@ try {
 			FILETIME.ptr,
 			FILETIME.ptr);
 
+	// GetSystemTimes
+	var fnGetSystemTimes = kernel32.declare('GetSystemTimes',
+			ctypes.winapi_abi,
+			ctypes.uint32_t,
+			FILETIME.ptr,
+			FILETIME.ptr,
+			FILETIME.ptr);
+
 	// GetSystemInfo
 	var fnGetSystemInfo = kernel32.declare('GetSystemInfo',
 			ctypes.winapi_abi,
@@ -112,6 +153,14 @@ try {
 			ctypes.uint32_t,
 			ctypes.voidptr_t,
 			PROCESS_MEMORY_COUNTERS.ptr,
+			ctypes.uint32_t);
+
+	// GetIfTable
+	var fnGetIfTable = iphlpapi.declare('GetIfTable',
+			ctypes.winapi_abi,
+			ctypes.uint32_t,
+			MIB_IFTABLE.ptr,
+			ctypes.uint32_t.ptr,
 			ctypes.uint32_t);
 
 
@@ -160,15 +209,8 @@ try {
 				fnGetProcessTimes(currentProcess,
 					ftdmy1.address(), ftdmy2.address(),
 					fxKtNow.address(), fxUtNow.address());
-				// logger.logStringMessage(' -----------------');
 				fxCpuUsage = getValueFromFileTime(fxKtNow) - getValueFromFileTime(fxKt) +
 					getValueFromFileTime(fxUtNow) - getValueFromFileTime(fxUt);
-				// logger.logStringMessage(' +++++++++++++++++');
-				/*
-				logger.logStringMessage('=============    ' + fxKtNow.ullDateTime + ':' + fxKt.ullDateTime + ' >>> ' + ts + ':' +  fxTs + '     ===============');
-				logger.logStringMessage('-------------    ' + fxUtNow.ullDateTime + ':' + fxUt.ullDateTime + ' >>> ' + ts + ':' +  fxTs + '     ===============');
-				*/
-				// logger.logStringMessage('-----------   ' + fxCpuUsage + '   ----------');
 				fxCpuUsage /= 100;
 				fxCpuUsage /= (ts - fxTs);
 				fxCpuUsage = Math.floor(fxCpuUsage / cpuCount);
@@ -190,26 +232,167 @@ try {
 		}
 	}
 
+
+	var sysFt = FILETIME();
+	var sysFtNow = FILETIME();
+	fnGetSystemTimes(sysFt.address(), ftdmy1.address(), ftdmy2.address());
+	var sysTs = Date.now();
+	var sysCpuUsage = 0;
 	this.GetSysCpuUsage = function(sys_cpu_usage) {
-		sys_cpu_usage.value = 50;
+		try {
+			var ts = Date.now();
+			if (ts - sysTs > 500) {
+				fnGetSystemTimes(sysFtNow.address(), ftdmy1.address(), ftdmy2.address());
+				sysCpuUsage = getValueFromFileTime(sysFtNow) - getValueFromFileTime(sysFt);
+				sysCpuUsage /= 100;
+				sysCpuUsage /= (ts - sysTs);
+				sysCpuUsage = Math.floor(sysCpuUsage / cpuCount);
+
+				if (sysCpuUsage > 100) {
+					sysCpuUsage = 100;
+				}
+	
+				sysTs = ts;
+				sysFt.dwLowDateTime = sysFtNow.dwLowDateTime;
+				sysFt.dwHighDateTime = sysFtNow.dwHighDateTime;
+			}
+	
+			sys_cpu_usage.value = 100 - sysCpuUsage;
+		} catch (e) {
+			logger.logStringMessage('vvvvvvvvvv' + e + 'vvvvvvvvvv');
+		}
 	}
 
 
 	////////////////////////////////////////////////
 	// Ethernet
 
+	/* below adapted from Ipifcons.h */
+	const MIB_IF_TYPE_OTHER              = 1;
+	const MIB_IF_TYPE_ETHERNET           = 6;
+	const MIB_IF_TYPE_TOKENRING          = 9;
+	const MIB_IF_TYPE_FDDI               = 15;
+	const MIB_IF_TYPE_PPP                = 23;
+	const MIB_IF_TYPE_LOOPBACK           = 24;
+	const MIB_IF_TYPE_SLIP               = 28;
+	const IF_TYPE_IEEE80211              = 71;
+
+	const IF_OPER_STATUS_NON_OPERATIONAL = 0;
+	const IF_OPER_STATUS_UNREACHABLE     = 1;
+	const IF_OPER_STATUS_DISCONNECTED    = 2;
+	const IF_OPER_STATUS_CONNECTING      = 3;
+	const IF_OPER_STATUS_CONNECTED       = 4;
+	const IF_OPER_STATUS_OPERATIONAL     = 5;
+
+
+	var iftable = MIB_IFTABLE();
+	iftable.dwNumEntries = 64;
+	var numEntries = ctypes.uint32_t(64);
+
+	var ethernetEntry = [];
+	var ethernetTick = 0;
 	this.GetEthernetCount = function() {
-		return 1;
+		updateEthernet();
+		return ethernetEntry.length;
 	}
 
 	this.GetEthernetSpeed = function(index, in_speed, out_speed) {
-		in_speed.value = 1024 * 24;
-		out_speed.value = 1024 * 32;
+		updateEthernet();
+		if (index >= 0 && index < ethernetEntry.length) {
+			var entry = ethernetEntry[index];
+			in_speed.value = entry.in_speed;
+			out_speed.value = entry.out_speed;
+		}
 	}
 
 	this.GetEthernetName = function(index, name) {
-		name.value = 'dummy!';
+		updateEthernet();
+		if (index >= 0 && index < ethernetEntry.length) {
+			name.value = ethernetEntry[index].name;
+		}
 	}
+
+	var ethernetUpdateTime = 0;
+	function updateEthernet() {
+		var updateTime = Date.now();
+		if (updateTime - ethernetUpdateTime < 500) {
+			return;
+		} else{
+			ethernetUpdateTime = updateTime;
+		}
+
+		fnGetIfTable(iftable.address(), numEntries.address(), 1);
+		var count = 0;
+		for (var i = 0; i < iftable.dwNumEntries; ++ i) {
+			var row = iftable.table[i];
+			if (filterRow(row)) {
+				++ count;
+			}
+		}
+
+		// reset entries
+		if (count != ethernetEntry.length) {
+			ethernetEntry = [];
+			ethernetTick = 0;
+			for (var i = 0; i < count; ++ i) {
+				ethernetEntry.push(
+					{
+						in_octets: 0,
+						out_octets: 0,
+						in_speed: 0,
+						out_speed: 0
+					});
+			}
+		}
+
+		count = 0;
+		var tick = updateTime;
+		var t = tick - ethernetTick;
+		if (t <= 0) {
+			t = 1;
+		}
+		for (var i = 0, j = 0; i < iftable.dwNumEntries; ++ i) {
+			var row = iftable.table[i];
+			if (!filterRow(row)) {
+				continue;
+			}
+
+			var entry = ethernetEntry[j];
+			if (ethernetTick > 0) {
+				entry.in_speed = Math.floor((row.dwInOctets - entry.in_octets) * 1000 / t);
+				entry.out_speed = Math.floor((row.dwOutOctets - entry.out_octets) * 1000 / t);
+			}
+
+			entry.in_octets = row.dwInOctets;
+			entry.out_octets = row.dwOutOctets;
+			if (entry.name === undefined) {
+				entry.name = '';
+				var n = row.dwDescrLen;
+				if (n > 1) {
+					-- n;
+				}
+				for (var k = 0; k < n; ++ k) {
+					entry.name += String.fromCharCode(row.bDescr[k]);
+				}
+			}
+
+			++ j;
+			if (j == count) {
+				break;
+			}
+		}
+		ethernetTick = tick;
+	}
+
+
+	function filterRow (row) {
+		var dwType = row.dwType;
+		var dwOperStatus = row.dwOperStatus;
+		return ((dwType == MIB_IF_TYPE_ETHERNET || dwType == MIB_IF_TYPE_PPP || dwType == MIB_IF_TYPE_FDDI || dwType == IF_TYPE_IEEE80211) &&
+			(dwOperStatus == IF_OPER_STATUS_OPERATIONAL || dwOperStatus == IF_OPER_STATUS_CONNECTED) &&
+			(row.dwInOctets > 0 && row.dwOutOctets > 0));
+	}
+
 
 } catch (e) {
 	logger.logStringMessage('***********' + e + '**********');
